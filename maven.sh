52417dcee63a0b9229d355b42f5aaac27de2380e
khaledklai <59127946+khaledklai@users.noreply.github.com>
2021-12-06 17:24:38 +0100

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,4 +1,4 @@
-echo 'export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk' > maven.sh
-echo 'export M2_HOME=/opt/maven' >> maven.sh
-echo 'export MAVEN_HOME=/opt/maven' >> maven.sh
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> maven.sh
+export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
